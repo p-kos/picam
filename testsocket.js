5b9e4a9abd8e336f2fc936aaeb1ec013d5d52a63
Marco Zarate <pkitos@gmail.com>
2015-09-26 07:44:34 -0400

    added style to selected thumbnail

--- testsocket.js
+++ testsocket.js
@@ -1,3 +1,15 @@
-/**
- * Created by p_kos on 9/21/15.
- */
+
+var args = process.argv[0];
+var io = require('socket.io-client');
+var socket=io('http://localhost:3705');
+
+//socket.emit('refreshImg', 'test 3 sent');
+socket.emit('refreshImg', process.argv[2]);
+setTimeout(function(){
+  socket.disconnect();
+}, 500);
+//socket.disconnect();
+
+socket.on('disconnect', function(){
+  console.log('disconnected from socket server');
+});
